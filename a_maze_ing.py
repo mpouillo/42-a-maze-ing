9fcef098b8ba28c7f70b091624d4ac637bfe4d81
Marceau Pouilloux <mpouillo@student.42lyon.fr>
2026-03-12 10:16:10 +0100

    added checks for config file existence + perms

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 from src import Application
 from dotenv import load_dotenv
 
@@ -9,6 +10,10 @@ def get_config_file() -> str:
     if len(sys.argv) != 2:
         sys.exit("Incorrect usage. "
                  "Run with: 'python3 a_maze_ing.py [config_file]")
+    if not os.access(sys.argv[1], os.F_OK):
+        sys.exit("Error: Config file not found")
+    if not os.access(sys.argv[1], os.R_OK):
+        sys.exit("Error: Missing permissions to read config file")
     return sys.argv[1]
 
 
@@ -22,5 +27,6 @@ def main() -> None:
 if __name__ == "__main__":
     try:
         main()
-    except (SystemExit, KeyboardInterrupt) as e:
+    except Exception as e:
         print("Exiting program:", e)
+        os._exit(1)
